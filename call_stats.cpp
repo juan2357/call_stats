1a750cdab5cbdde71600800e3793e052f7ded36f
juanfperez <juanfperez@me.com>
2018-01-30 12:48:10 -0500

    declared call record

--- call_stats.cpp
+++ call_stats.cpp
@@ -9,3 +9,17 @@ Description: This is a program that calculates the net cost of a call (net_cost)
               the tax on a call (call_tax)
               and the total cost of the call (total_cost).
 *************************************************************/
+/*********************************************************
+//Following is the declaration of a call record
+**********************************************************/
+class call_record
+{
+public:
+	string cell_number;
+	int relays;
+	int call_length;
+	double net_cost;
+	double tax_rate;
+	double call_tax;
+	double total_cost;
+};
