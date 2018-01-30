d400423e6d2936a736beffcea1d2f19f5633caef
juanfperez <juanfperez@me.com>
2018-01-30 12:45:27 -0500

    data file added

---
+++ call_stats.cpp
@@ -0,0 +1,11 @@
+/***********************************************************
+Name:  Juan Perez   Z#: 23026404
+Course: Foundations of Computer Science (COP3014)
+Professor: Dr. Lofton Bullard
+Due Date: 01/30/18      Due Time: 11:59 PM
+Total Points: 20     Assignment 1: call_stats
+
+Description: This is a program that calculates the net cost of a call (net_cost),
+              the tax on a call (call_tax)
+              and the total cost of the call (total_cost).
+*************************************************************/
